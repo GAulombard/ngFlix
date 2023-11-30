8200d0e805f83a911401b533bb3696960a0582b5
Hodor <geoffrey.aulombard@gmail.com>
2023-11-30 23:53:25 +0100

    tuto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ngFlix';
+  title = 'ngFlix'
 }
